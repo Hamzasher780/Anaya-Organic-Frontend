7b6351220c00217adbfab2ded6c9b882f6676b4e
Hamzasher780 <Hamzasher780@gmail.com>
2024-09-07 15:27:14 +0500

    making footer || admin navbars adjustments

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://anaya-organic-backend.onrender.com'  // Use the hosted backend URL
+    API_URL: 'https://anayaorganic.netlify.app'  // Use the hosted backend URL
 };
 
 // Expose the config object globally so it can be accessed in other scripts
